9755bd461dfef4e4b5250b9dc3c4205b5f2bb024
preethichals <108888412+preethichals@users.noreply.github.com>
2024-01-24 15:09:19 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
 <footer className=" bg-neutral-900 container mx-auto max-w-full shadow dark:bg-gray-900">
   <div className="w-full max-w-screen-xl mx-auto p-4 text-slate-50">
     <div className="sm:flex sm:items-center sm:justify-between">
-         <span className="block text-sm  sm:text-center dark:text-gray-400"><a href="mailto:preethi.lyn@gmail.com" className="hover:underline"> Created by Preethi-- © 2023 </a><a href="mailto:peterindia@gmail.com" className="hover:underline">PeterIndia™</a>. All Rights Reserved.</span>
+         <span className="block text-sm  sm:text-center dark:text-gray-400"><a href="mailto:preethi.lyn@gmail.com" className="hover:underline"> Created by Preethi-- © 2024 </a><a href="mailto:peterindia@gmail.com" className="hover:underline">PeterIndia™</a>. All Rights Reserved.</span>
 
       <ul className="flex flex-wrap items-center mb-6 text-sm font-medium tracking-widest uppercase sm:mb-0 dark:text-gray-400">
          <li>
